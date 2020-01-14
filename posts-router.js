a3e6abe4780f472c1635f3065719613f8a3564fa
AbdelI <homeworkoracle@protonmail.com>
2020-01-14 17:06:44 +0100

    -

--- posts-router.js
+++ posts-router.js
@@ -73,3 +73,60 @@ router.delete("/:id", (req, res) => {
       });
     });
 });
+
+// router.put("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const changes = req.body;
+//     Posts.update(req.params.id, changes)
+//         .then(post => {
+//             if (changes.title && changes.contents) {
+//                 res.status
+
+//             } else {
+
+//       }
+//   });
+// });
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Posts.update(id, changes)
+    .then(post => {
+      //     if (!changes.title || !changes.contents) {
+      //       return res.status(400).json({
+      //         errorMessage: "Please provide title and contents for the post."
+      //       });
+      //     }
+      //     Posts.findById(id)
+      //       .then(post => {
+      //         if (post) {
+      //           return Posts.update(id, changes);
+      //         } else {
+      //           res.status(404).json({
+      //             message: "The post with the specified ID does not exist."
+      //           });
+      //         }
+      //       })
+      //       .then(() => Posts.findById(id))
+      //       .then(post => res.status(200).json(post))
+      //       .catch(error => {
+      //         res.status(500).json({
+      //           error: "The post information could not be modified."
+      //         });
+      //       });
+      const idcheck = Posts.findById(id);
+
+      if (changes.title && idcheck && changes.contents) {
+        res.status(200).json(post);
+      } else if (changes.title && changes.contents) {
+        return res.status(404).json({
+          message: "not working"
+        });
+      } else return res.status(400).json({ message: "bad" });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "not cool 500" });
+    });
+});
