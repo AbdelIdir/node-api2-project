c6769c07e0a600849462d72fb348702700b2ab96
AbdelI <homeworkoracle@protonmail.com>
2020-01-14 14:33:08 +0100

    delete request working and takes out the post with the specified id

--- posts-router.js
+++ posts-router.js
@@ -25,7 +25,7 @@ router.get("/:id", (req, res) => {
       if (posts) {
         res.status(200).json(posts);
       } else {
-        res.status(400).json({
+        res.status(404).json({
           message: "The post with the specified ID does not exist."
         });
       }
@@ -43,7 +43,7 @@ router.get("/:id/comments", (req, res) => {
       if (postsComment) {
         res.status(200).json(postsComment);
       } else {
-        res.status(400).json({
+        res.status(404).json({
           message: "The post with the specified ID does not exist."
         });
       }
@@ -52,6 +52,24 @@ router.get("/:id/comments", (req, res) => {
     .catch(error => {
       res.status(500).json({
         error: "The post information could not be retrieved."
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Posts.remove(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(201).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The post could not be removed"
       });
     });
 });
